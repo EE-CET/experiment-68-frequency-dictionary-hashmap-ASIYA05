4cfdcab146d11954411a3767a2208ce1c9ec9bd9
ASIYA05 <tve24el013@cet.ac.in>
2026-02-27 10:50:14 +0530

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -6,18 +6,13 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String line = scanner.nextLine();
-
+        String line = scanner.nextLine().trim();
         String[] words = line.split("\\s+");
 
         HashMap<String, Integer> map = new HashMap<>();
 
         for (String word : words) {
-            if (map.containsKey(word)) {
-                map.put(word, map.get(word) + 1);
-            } else {
-                map.put(word, 1);
-            }
+            map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
